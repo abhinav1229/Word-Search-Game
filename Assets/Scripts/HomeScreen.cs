4be8f3191cd21b5d778e7ab9bda72003f36f3272
Abhinav <rk7772534@gmail.com>
2024-11-17 12:44:48 +0530

    Add Reset game popup + Dotween Plugin + Settings

--- Assets/Scripts/HomeScreen.cs
+++ Assets/Scripts/HomeScreen.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HomeScreen : MonoBehaviour
 {
     [SerializeField] private Text _starCountText, _levelText;
+    [SerializeField] RectTransform _resetWindow;
 
     private void OnEnable()
     {
@@ -19,5 +21,24 @@ public class HomeScreen : MonoBehaviour
         Instantiate(gameScreen, GameData.Instance.MainCanvas.transform);
 
         Destroy(gameObject);
+    }
+
+    public void OnResetGameButtonClick(GameObject resetButton)
+    {
+        _resetWindow.DOAnchorPos(new Vector3(0, 300f, 0), 0.3f).SetEase(Ease.InFlash);
+    }
+
+    public void OnYesButtonClick(GameObject yesButton)
+    {
+        PlayerPrefs.DeleteAll();
+        _levelText.text = "Level " + GameData.UnlockedLevel.ToString();
+        _starCountText.text = GameData.StarsCount.ToString();
+
+        _resetWindow.DOAnchorPos(new Vector3(0, -700f, 0), 0.5f).SetEase(Ease.InFlash);
+    }
+
+    public void OnNoButtonClick(GameObject noButton)
+    {
+        _resetWindow.DOAnchorPos(new Vector3(0, -700f, 0), 0.5f).SetEase(Ease.InFlash);
     }
 }
