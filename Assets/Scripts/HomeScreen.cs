a3e16ef6f79d254593442a1fb1cb45238e1d61fc
Abhinav <rk7772534@gmail.com>
2024-11-19 01:20:27 +0530

    Add Developer Button and window

--- Assets/Scripts/HomeScreen.cs
+++ Assets/Scripts/HomeScreen.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.Networking;
 
 public class HomeScreen : MonoBehaviour
 {
     [SerializeField] private Text _starCountText, _levelText;
-    [SerializeField] RectTransform _resetWindow;
+    [SerializeField] RectTransform _resetWindow, _developerWindow;
 
     public static HomeScreen Instance;
 
@@ -29,6 +30,7 @@ public class HomeScreen : MonoBehaviour
     {
         GameData.Instance.ObjectScaleAnimation(playButton);
         _resetWindow.DOAnchorPos(new Vector3(0, -700f, 0), 0.5f).SetEase(Ease.InFlash);
+        _developerWindow.DOAnchorPos(new Vector3(0, -1000f, 0), 0.5f).SetEase(Ease.InFlash);
 
         transform.DOScale(Vector3.one, 0f).SetDelay(0.5f).OnComplete(() =>
         {
@@ -75,6 +77,46 @@ public class HomeScreen : MonoBehaviour
         transform.DORotate(Vector3.zero, 0).SetDelay(0.5f).OnComplete(() =>
         {
             _resetWindow.DOAnchorPos(new Vector3(0, -700f, 0), 0.5f).SetEase(Ease.InFlash);
+        });
+    }
+
+    public void OnDeveloperButtonClick(GameObject devButton)
+    {
+        GameData.Instance.ObjectScaleAnimation(devButton);
+        AudioManager.Instance.PlayButtonClickSound();
+        transform.DORotate(Vector3.zero, 0).SetDelay(0.5f).OnComplete(() =>
+        {
+            _developerWindow.DOAnchorPos(new Vector3(0, 420f, 0), 0.5f).SetEase(Ease.InFlash);
+        });
+    }
+
+    public void OnDeveloperCloseButtonClick(GameObject devCloseButton)
+    {
+        GameData.Instance.ObjectScaleAnimation(devCloseButton);
+        AudioManager.Instance.PlayButtonClickSound();
+        transform.DORotate(Vector3.zero, 0).SetDelay(0.5f).OnComplete(() =>
+        {
+            _developerWindow.DOAnchorPos(new Vector3(0, -1000f, 0), 0.5f).SetEase(Ease.InFlash);
+        });
+    }
+
+
+    public void OnMailButtonClick(GameObject mailButton)
+    {
+        GameData.Instance.ObjectScaleAnimation(mailButton);
+        AudioManager.Instance.PlayButtonClickSound();
+        transform.DORotate(Vector3.zero, 0).SetDelay(0.5f).OnComplete(() =>
+        {
+            // Replace with your desired email address
+            string email = "abhinavrajat1229@gmail.com";
+            string subject = "Hi Abhinav, Happy to connect with you.";
+            string body = "";
+
+
+            // Construct the mailto URL
+            string mailto = "mailto:" + email + "?subject=" + UnityWebRequest.EscapeURL(subject) + "&body=" + UnityWebRequest.EscapeURL(body);
+
+            Application.OpenURL(mailto);
         });
     }
 }
